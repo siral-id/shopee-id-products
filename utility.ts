beb41b11a542e5f504003fc1c4457627e8480c18
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-05-21 20:37:00 +0700

    test new setup

--- utility.ts
+++ utility.ts
@@ -1,13 +1,22 @@
 import { readJSON, writeJSON } from "https://deno.land/x/flat/mod.ts";
+import {existsSync} from "https://deno.land/std/fs/mod.ts";
+
 
 export function sleep(seconds: number) {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
 export async function appendJSON<T>(filepath: string, data: T[]) {
-  const currentData = await readJSON(filepath);
-  const combinedData: T[] = currentData.concat(data);
-  const timestamp = new Date().toISOString();
-  combinedData.map((obj) => ({ ...obj, timestamp }));
+  const isExist = existsSync(filepath)
+  let combinedData: T[];
+  if(isExist){
+    const currentData = await readJSON(filepath);
+    combinedData = currentData.concat(data);
+    const timestamp = new Date().toISOString();
+    combinedData.map((obj) => ({ ...obj, timestamp }));
+  }else{
+    combinedData = data;
+  }
+
   await writeJSON(filepath, combinedData);
 }
