c36dffacbbe54cac818d226d21fc12d77c7e9535
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-01 15:49:59 +0700

    chore: integrate mongo

---
+++ daily_products.ts
@@ -0,0 +1,40 @@
+import {
+  IShopeeProductResponse,
+  IShopeeProductSchema,
+} from "./interfaces/mod.ts";
+import {
+  sleep,
+} from "https://raw.githubusercontent.com/siral-id/deno-utility/main/utility.ts";
+import { getMongoClient } from "https://raw.githubusercontent.com/siral-id/deno-utility/main/database.ts";
+
+const noOfPages = 10;
+const sleepDuration = 0.1;
+
+const mongoUri = Deno.env.get("MONGO_URI");
+if (!mongoUri) throw new Error("MONGO_URI not found");
+
+const client = await getMongoClient(mongoUri);
+const collection = client.database().collection<IShopeeProductSchema>(
+  "products",
+);
+
+const offsets = Array.from(Array(noOfPages).keys());
+
+await Promise.all(offsets.map(async (offset) => {
+  const url =
+    `https://shopee.co.id/api/v4/recommend/recommend?bundle=daily_discover_main&item_card=${offset}&limit=60&offset=${offset}`;
+  const response = await fetch(url);
+  const data: IShopeeProductResponse = await response.json();
+
+  const section = data["data"]["sections"][0];
+  const products = section["data"]["item"];
+
+  const productsWithSource = products.map((product) => ({
+    ...product,
+    source: "SHOPEE",
+  }));
+  await collection.insertMany(productsWithSource);
+
+  // prevent hammering the api source
+  await sleep(sleepDuration);
+}));
---
+++ interfaces/mod.ts
@@ -0,0 +1,2 @@
+export * from "./product.ts"
+export * from "./search.ts"
