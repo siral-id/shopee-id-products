1159836c5237806125be4a7dc809fb7fef0fcff3
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-10 13:16:41 +0700

    feat: calculate chunk size

--- cli/daily_products.ts
+++ cli/daily_products.ts
@@ -12,11 +12,11 @@ const octokit = setupOctokit(ghToken);
 const response = await fetchDailyProducts();
 
 await Promise.all(
-  chunkItems(response, 256).map(async (chunk) => {
+  chunkItems(response).map(async (chunk) => {
     await upload<ICreateProductWithImages[]>(
       octokit,
       chunk,
-      "WRITE_SHOPEE_PRODUCTS",
+      "WRITE_PRODUCTS_SHOPEE",
     );
   }),
 );
